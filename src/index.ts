56f18b8876523d16cb82c4df0ccf56fabcec4b84
Carolina Rojas <carorojas27@gmail.com>
2025-01-02 11:02:59 -0300

    firs commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { pool } from "./config/database";
+import app from "./app"
+
+const port = process.env.PORT || 3000;
+
+(async () => {
+    try {
+        const { rows } = await pool.query("SELECT NOW()");
+        console.log(`DB connecting: ${rows[0].now}`);
+        app.listen(port, () => {
+            console.log("Servidor andando en el puerto: " + port);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})();
